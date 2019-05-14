c0ba2dec0fbf54cc93fa2d18e897fb5a8593ed71
mjaycub <themarkjacob@gmail.com>
2019-05-14 15:36:37 +0100

    Modified JSON. Now accessing array within the JSON object.

--- test.py
+++ test.py
@@ -4,4 +4,14 @@ from pprint import pprint
 with open('data.json') as file:
 	data = json.load(file)
 
-pprint(data)
+# pprint(data)
+
+# Access the 'data' object and get the 'transcript' attribute
+storyTranscript = data["transcript"]
+pprint(storyTranscript)
+
+# Remember arrays work on "zero-based indexing/numbering"
+# This means that the initial element is assigned the index 0
+# To access the FIRST word in our words array, we access data["words"][0]
+firstWord = data["words"][0]["word"]
+pprint("The first word is: " + firstWord)
